1a648fcfd45d4a644b4ff8dcf4b0e4efa7bda187
Tadeas Petak <tadeas@donna.legal>
2021-12-02 10:50:38 +0100

    general setup

---
+++ src/app.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/api/contents");
+      const json = await response.json();
+    })();
+  }, []);
+
+  return (
+    <div className="App bg-white">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload. Me
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
