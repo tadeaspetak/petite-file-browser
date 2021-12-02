39e308f3836d48162eb0c402479343e8d5f36520
Tadeas Petak <tadeaspetak@gmail.com>
2021-12-02 15:46:10 +0100

    setup api tests

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 import fs from "fs";
 import path from "path";
@@ -5,8 +6,9 @@ import path from "path";
 import { api } from "./api";
 
 const app = express();
+// app.disable('x-powered-by') TODO
 app.use(express.json());
-
+app.use(cookieParser(""));
 app.use("/api", api);
 
 const client = path.resolve(__dirname, "../build");
